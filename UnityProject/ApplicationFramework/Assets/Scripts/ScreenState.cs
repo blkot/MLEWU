83f0106736725b34a6735b4a0930dedcd35430cc
WIN-G12EGIPUNQ3\Administrator <tom.wang@digitwin.com.cn>
2023-05-30 03:51:42 +0800

    add first screenstate/stategeneric script.

---
+++ UnityProject/ApplicationFramework/Assets/Scripts/ScreenState.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Xanta.Application.Framework.UI.Core
+{
+    public abstract class ScreenState : MonoBehaviour
+    {
+        public abstract void Initialize();
+        public abstract void Show();
+        public abstract void Hide();
+    }
+}
---
+++ UnityProject/ApplicationFramework/Assets/Scripts/ScreenStateGeneric.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace Xanta.Application.Framework.UI.Core
+{
+    public abstract class ScreenState<TState, TView> : ScreenState
+        where TState : ScreenState where TView : MonoBehaviour
+    {
+        private const string VIEWS_ADDRESS_IN_RESOURCE = "Views/";
+
+        #region Singleton
+
+        private static TState instance;
+        public static TState Instance => instance ?? InitInstance();
+
+        private static TState InitInstance()
+        {
+            instance = new GameObject(typeof(TState).Name).AddComponent<TState>();
+            instance.Initialize();
+            return instance;
+            // throw new System.NotImplementedException();
+        }
+
+        #endregion
+
+        protected TView view;
+
+        protected virtual string ViewAddress => VIEWS_ADDRESS_IN_RESOURCE + typeof(TView).Name;
+
+        public override void Initialize()
+        {
+            view = Instantiate(Resources.Load<GameObject>(VIEWS_ADDRESS_IN_RESOURCE).GetComponent<TView>());
+            throw new System.NotImplementedException();
+        }
+
+        public override void Show()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Hide()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
